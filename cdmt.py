209f6292cbc85edda0492a6bf1663e87fe482697
willewille <56308919+willewille@users.noreply.github.com>
2022-07-22 17:44:44 +0200

    Update cdmt.py

--- cdmt.py
+++ cdmt.py
@@ -5,3 +5,16 @@ Motor1A = 27
 Motor1B = 22
 Motor1E = 17
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Motor1A,GPIO.OUT)
+GPIO.setup(Motor1B,GPIO.OUT)
+GPIO.setup(Motor1E,GPIO.OUT)
+
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1A,GPIO.LOW)
+
+GPIO.output(Motor1A,GPIO.HIGH)
+GPIO.output(Motor1A,GPIO.LOW)
+GPIO.output(Motor1A,GPIO.HIGH)
